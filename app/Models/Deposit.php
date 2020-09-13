b5072db09b7cee1fcf052e8dc64a01021b84bbc6
root <slavko.alexandr@gmail.com>
2020-09-13 11:20:19 +0000

    Models fillable is complete

--- app/Models/Deposit.php
+++ app/Models/Deposit.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Deposit extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'wallet_id', 'invested', 'percent', 'active', 'duration', 'accrue_times'];
 }
--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['type', 'user_id', 'wallet_id', 'deposit_id', 'amount'];
 }
